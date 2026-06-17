6840445ff57fef174964c7894f826a241002b66f
mackenzievich <mackenzievich@gmai.com>
2026-06-17 13:55:20 +0300

    finish sql

--- src/main/java/ru/yandex/practicum/filmorate/dao/FriendDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FriendDbStorage.java
@@ -17,13 +17,13 @@ public class FriendDbStorage implements FriendStorage {
     private final JdbcTemplate jdbcTemplate;
     private final SqlFileReader fileReader;
 
-    private  String addFriendSql;
-    private  String removeFriendSql;
-    private  String checkCountSql;
-    private  String updateStatusTrueSql;
-    private  String updateStatusFalseSql;
-    private  String findAllFriendsSql;
-    private  String findCommonFriendsSql;
+    private String addFriendSql;
+    private String removeFriendSql;
+    private String checkCountSql;
+    private String updateStatusTrueSql;
+    private String updateStatusFalseSql;
+    private String findAllFriendsSql;
+    private String findCommonFriendsSql;
 
 
     @PostConstruct
@@ -36,53 +36,54 @@ public class FriendDbStorage implements FriendStorage {
         findAllFriendsSql = fileReader.readSqlFile("/friend/findAllFriends.sql");
         findCommonFriendsSql = fileReader.readSqlFile("/friend/findCommonFriends.sql");
     }
-        @Override
-        public void addFriend ( int id, int friendId){
-            String sql = addFriendSql;
-            jdbcTemplate.update(sql, id, friendId);
-            checkStatus(id, friendId);
-        }
 
-        @Override
-        public void removeFriend ( int id, int friendId){
-            String sql = removeFriendSql;
-            jdbcTemplate.update(sql, id, friendId);
-            checkStatus(id, friendId);
-        }
+    @Override
+    public void addFriend(int id, int friendId) {
+        String sql = addFriendSql;
+        jdbcTemplate.update(sql, id, friendId);
+        checkStatus(id, friendId);
+    }
 
-        private void checkStatus ( int userId, int friendId){
-            String checkSql = checkCountSql;
-            int count = jdbcTemplate.queryForObject(checkSql, Integer.class, friendId, userId);
+    @Override
+    public void removeFriend(int id, int friendId) {
+        String sql = removeFriendSql;
+        jdbcTemplate.update(sql, id, friendId);
+        checkStatus(id, friendId);
+    }
 
-            String updateSql;
-            if (count > 0) {
-                updateSql = updateStatusTrueSql;
-                jdbcTemplate.update(updateSql, userId, friendId, friendId, userId);
-            } else {
-                updateSql = updateStatusFalseSql;
-                jdbcTemplate.update(updateSql, userId, friendId);
-            }
-        }
+    private void checkStatus(int userId, int friendId) {
+        String checkSql = checkCountSql;
+        int count = jdbcTemplate.queryForObject(checkSql, Integer.class, friendId, userId);
 
-        @Override
-        public List<User> findAllFriends ( int id){
-            String sql = findAllFriendsSql;
-            return jdbcTemplate.query(sql, (rs, rowNum) -> makeFriend(rs), id);
-        }
-
-        @Override
-        public List<User> findCommonFriends ( int id, int otherId){
-            String sql = findCommonFriendsSql;
-            return jdbcTemplate.query(sql, (rs, rowNum) -> makeFriend(rs), id, otherId);
-        }
-
-        private User makeFriend (ResultSet rs) throws SQLException {
-            return User.builder()
-                    .id(rs.getInt("user_id"))
-                    .email(rs.getString("email"))
-                    .login(rs.getString("login"))
-                    .name(rs.getString("name"))
-                    .birthday(rs.getDate("birthday").toLocalDate())
-                    .build();
+        String updateSql;
+        if (count > 0) {
+            updateSql = updateStatusTrueSql;
+            jdbcTemplate.update(updateSql, userId, friendId, friendId, userId);
+        } else {
+            updateSql = updateStatusFalseSql;
+            jdbcTemplate.update(updateSql, userId, friendId);
         }
     }
+
+    @Override
+    public List<User> findAllFriends(int id) {
+        String sql = findAllFriendsSql;
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeFriend(rs), id);
+    }
+
+    @Override
+    public List<User> findCommonFriends(int id, int otherId) {
+        String sql = findCommonFriendsSql;
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeFriend(rs), id, otherId);
+    }
+
+    private User makeFriend(ResultSet rs) throws SQLException {
+        return User.builder()
+                .id(rs.getInt("user_id"))
+                .email(rs.getString("email"))
+                .login(rs.getString("login"))
+                .name(rs.getString("name"))
+                .birthday(rs.getDate("birthday").toLocalDate())
+                .build();
+    }
+}
